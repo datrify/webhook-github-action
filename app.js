6f5349ed5985d18e43dd9f1ee7106340fa23e200
Datrify <101674548+datrify@users.noreply.github.com>
2025-11-07 22:44:25 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+{
+  "object": "whatsapp_business_account",
+  "entry": [
+    {
+      "id": "2269622793483088",
+      "changes": [
+        {
+          "value": {
+            "messaging_product": "whatsapp",
+            "metadata": {
+              "display_phone_number": "18723287439",
+              "phone_number_id": "1119411200370670"
+            },
+            "contacts": [
+              {
+                "profile": {
+                  "name": "Datrify"
+                },
+                "wa_id": "18723287439"
+              }
+            ],
+            "messages": [
+              {
+                "from": "18723287439",
+                "id": "wamid.HBgLMTc4NjM1NTk5NjYVAGHAYWYET688aASGNTI1QzZFQjhEMDk2QQA=",
+                "timestamp": "1758254144",
+                "text": {
+                  "body": "Hi!"
+                },
+                "type": "text"
+              }
+            ]
+          },
+          "field": "messages"
+        }
+      ]
+    }
+  ]
+}
+
